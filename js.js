6dcb863619d39e32ce9a0a2cd90d5d461fed91ed
salvador <s.pamanes@gmail.com>
2015-10-15 14:40:07 -0500

    sldif

--- js.js
+++ js.js
@@ -5,24 +5,24 @@ $( document ).ready(function() {
     $('#textarea1').keyup(function(event) {
         var code = (event.keyCode ? event.keyCode : event.which);
         if(code == 13){
-            writeTweet('YOU', $('#textarea1').val());
+            writeTweet(1234567, 'YOU', $('#textarea1').val());
             $('#textarea1').val('');
         }
     });
 
-    function writeTweet (user, text) {
+    function writeTweet (id, user, text) {
 
 
 
         //alert('si');
         $('#tweetCnt').prepend( '<div class="card white darken-1"><div class="card-content white-text"><table><tbody><tr><td><img id="image" width="80px" height="80px" src="http://www.vacul.org/extension/site/design/site//images/anonymous-user.png"></td><td style="width:100%;"><span class="card-title black-text" >' + user + '</span><p class="black-text">' + text + '</p><img src="tweet.png" ></td></tr></tbody></table></div></div>');
-    
+
      // $('#tweetCnt').prepend('<div class="row"><div class="col s12 m6 offset-m3"><div class="card blue darken-1"><div class="card-content white-text"><span class="card-title">'+
     //        user +'</span><p>' + text + '</p></div> <div class="card-action"><a href="#">Reply</a><a href="#">Retweet</a></div></div></div></div>');
     }
 
-    var interval = setInterval(writeTweet, 30000, 'adadawd', 'AOINADOINF');
-    
- 
-});
+    var interval = setInterval(writeTweet, 30000, 56789876, 'adadawd', 'AOINADOINF');
 
+
+
+});
--- tweets.sql
+++ tweets.sql
@@ -1,8 +1,19 @@
+create database tweeter;
 
+use tweeter;
 
-create table if not exists tweets ()
+create table if not exists tweets (
 	body varchar,
 	user varchar,
 	id int auto_increment,
 	primary key(id)
-)
+);
+
+insert into tweets(user, body)
+	values ('juan', 'just ate pixxa #pizza #yum #gluten');
+insert into tweets(user, body)
+	values ('memexia', 'ayo word man pizza dashet @juan');
+insert into tweets(user, body)
+	values ('andrewNinsaf', 'iz tha man against me all tha time were i get medicine #sick');
+insert into tweets(user, body)
+	values ('Presidentobama', 'hey putin gimme monies no syria 4 u #diplomacy');
